000e4c923e3703237baaafe6056ed88ab201319c
atakaamkaar <drkaamkaar76@gmail.com>
2025-06-13 20:26:12 +0330

    Setup for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
+import Shop from './pages/shop/shop';
+import Cart from './pages/cart/cart';
+import Nav from './components/nav';
+import context from 'react-bootstrap/esm/AccordionContext';
+import { ShopContextProvider } from './context/shopContext';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShopContextProvider>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
---
+++ src/context/shopContext.js
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+import { useCart } from "../hook/useCart";
+
+export const ShopContext = createContext({
+  cartItems: null,
+  addToCart: () => {},
+  removeFromCart: () => {}
+});
+export const ShopContextProvider = (props) => {
+  return (
+    <ShopContext.Provider value={useCart()}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
